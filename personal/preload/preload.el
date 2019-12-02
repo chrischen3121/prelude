481d733a1ec16d93323df13afd5baebc8cec41ec
chenyewei <chenyewei@huiyin.com>
2019-12-02 14:54:38 +0800

    add keybinding

--- personal/preload/preload.el
+++ personal/preload/preload.el
@@ -1,5 +1,2 @@
-(setq package-archives
-      '(("gnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-        ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
-        ("marmalade" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/marmalade/")
-        ("org" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")))
+(global-set-key (kbd "S-SPC") 'set-mark-command)
+(global-set-key (kbd "M-i") 'imenu)
