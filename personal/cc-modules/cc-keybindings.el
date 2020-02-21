779923d1bd3decd1b5da9e29c7756909c5c7140f
chenyewei <chenyewei@huiyin.com>
2020-02-21 18:12:59 +0800

    update keybindings

--- personal/cc-modules/cc-keybindings.el
+++ personal/cc-modules/cc-keybindings.el
@@ -6,5 +6,6 @@
 (global-unset-key (kbd "C-+"))
 (global-set-key (kbd "C-l") 'ivy-backward-delete-char)
 (global-set-key (kbd "S-SPC") 'set-mark-command)
+
 (global-set-key (kbd "M-i") 'imenu)
 (provide 'cc-keybindings)
