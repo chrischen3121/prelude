332b33fabca5d581f3ab316613afb7cf3c66c154
chenyewei <chenyewei@huiyin.com>
2020-05-19 15:03:53 +0800

    update yas config

--- personal/cc-modules/cc-dev.el
+++ personal/cc-modules/cc-dev.el
@@ -1,7 +1,9 @@
+;; yasnippet config
 (prelude-require-packages '(yasnippet yasnippet-snippets))
 (defconst cc-snippets-dir (expand-file-name "snippets" prelude-personal-dir))
 (push cc-snippets-dir yas-snippet-dirs)
 (yas-reload-all)
 (add-hook 'prog-mode-hook #'yas-minor-mode)
+(define-key prog-mode-map (kbd "M-/") 'yas-expand-from-trigger-key)
 
 (provide 'cc-dev)
