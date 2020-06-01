51725dbf428f252f42f5aec0b8798ab08e7c931e
chenyewei <chenyewei@huiyin.com>
2020-06-01 11:59:36 +0800

    add org snippets

--- personal/cc-modules/cc-better-defaults.el
+++ personal/cc-modules/cc-better-defaults.el
@@ -60,6 +60,7 @@
 (global-unset-key (kbd "M-s ESC"))
 
 (global-set-key (kbd "C--") 'negative-argument)
+(global-set-key (kbd "C-c C-c") 'org-capture)
 (global-set-key (kbd "M-\\") 'just-one-space)
 (global-unset-key (kbd "C-+"))
 (global-set-key (kbd "C-l") 'ivy-backward-delete-char)
--- personal/cc-modules/cc-dev.el
+++ personal/cc-modules/cc-dev.el
@@ -3,7 +3,16 @@
 (defconst cc-snippets-dir (expand-file-name "snippets" prelude-personal-dir))
 (push cc-snippets-dir yas-snippet-dirs)
 (yas-reload-all)
-(add-hook 'prog-mode-hook #'yas-minor-mode)
-(define-key prog-mode-map (kbd "M-/") 'yas-expand-from-trigger-key)
+
+
+
+(add-hook 'prog-mode-hook
+          (lambda ()
+            (yas-minor-mode-on)
+            (define-key prog-mode-map (kbd "M-/") 'yas-expand-from-trigger-key)))
+(add-hook 'org-mode-hook
+          (lambda ()
+            (yas-minor-mode-on)
+            (define-key org-mode-map (kbd "M-/") 'yas-expand-from-trigger-key)))
 
 (provide 'cc-dev)
