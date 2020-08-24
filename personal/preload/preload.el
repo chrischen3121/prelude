6f84d1dc9a4179fec18a99b0b2fa0352adb482d2
chrischen <chrischen3121@gmail.com>
2020-08-24 15:09:22 +0800

    change fontset

--- personal/preload/preload.el
+++ personal/preload/preload.el
@@ -2,3 +2,5 @@
 
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
+(add-to-list 'default-frame-alist '(font . "Source Code Pro-10"))
+(set-fontset-font t 'han "WenQuanYi Micro Hei Mono-11")
